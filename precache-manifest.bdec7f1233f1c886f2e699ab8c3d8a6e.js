df9b16a6a9b2593c85f803bf2d8f4d966c9b5874
vagon_straponov <Zelinov_1097@mail.ru>
2020-06-08 11:50:46 +0500

    Updates

---
+++ precache-manifest.bdec7f1233f1c886f2e699ab8c3d8a6e.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "120d68ab9c60d78bb583632060ed275a",
+    "url": "/master-zaim/index.html"
+  },
+  {
+    "revision": "980035711735cc6fcb07",
+    "url": "/master-zaim/static/css/2.bd130a2f.chunk.css"
+  },
+  {
+    "revision": "9e111877384b70dc584f",
+    "url": "/master-zaim/static/css/main.b64a6ac4.chunk.css"
+  },
+  {
+    "revision": "980035711735cc6fcb07",
+    "url": "/master-zaim/static/js/2.fc63fe87.chunk.js"
+  },
+  {
+    "revision": "3472dcbe06c35d80cac0ef4809bf5935",
+    "url": "/master-zaim/static/js/2.fc63fe87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e111877384b70dc584f",
+    "url": "/master-zaim/static/js/main.ab7986c2.chunk.js"
+  },
+  {
+    "revision": "c1ef1f97484a74163a2d",
+    "url": "/master-zaim/static/js/runtime-main.c06388c3.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/master-zaim/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/master-zaim/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "f3d5468e842cba474b12d95c301ba418",
+    "url": "/master-zaim/static/media/bg3.f3d5468e.png"
+  },
+  {
+    "revision": "bc1978b9b7f6c497b0e4f50752be2f3a",
+    "url": "/master-zaim/static/media/bg_2.bc1978b9.png"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/master-zaim/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/master-zaim/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/master-zaim/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/master-zaim/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/master-zaim/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/master-zaim/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/master-zaim/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/master-zaim/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/master-zaim/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/master-zaim/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/master-zaim/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/master-zaim/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/master-zaim/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/master-zaim/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/master-zaim/static/media/fa-solid-900.ec763292.svg"
+  },
+  {
+    "revision": "d402bd40d2244571c6ccbce75d508fb0",
+    "url": "/master-zaim/static/media/fisrtImg.d402bd40.png"
+  },
+  {
+    "revision": "33e3f4a7e3d816d64265d0a1fade884d",
+    "url": "/master-zaim/static/media/fourImg.33e3f4a7.png"
+  },
+  {
+    "revision": "f2da7e711f16575dae3e0ea737f90ddd",
+    "url": "/master-zaim/static/media/homeBg.f2da7e71.png"
+  },
+  {
+    "revision": "c875e48b12f3be0f6643aed101dead8b",
+    "url": "/master-zaim/static/media/logo2.c875e48b.png"
+  },
+  {
+    "revision": "b74513b529803ec7a13a6190d05ac1d9",
+    "url": "/master-zaim/static/media/secondImg.b74513b5.png"
+  },
+  {
+    "revision": "9570c113ff435a8b040f803c9b22de4a",
+    "url": "/master-zaim/static/media/ssl.9570c113.jpg"
+  },
+  {
+    "revision": "661022cc46533130650da863c91c607a",
+    "url": "/master-zaim/static/media/thirdImg.661022cc.png"
+  }
+]);
